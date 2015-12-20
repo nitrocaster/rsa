528aff900373cc6fddcb8fdfa90b1d11fe5fa0f4
nitrocaster <paul-kv@yandex.ru>
2015-12-20 20:43:44 +0300

    Add basic project files.

---
+++ src/rsa/common.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "config.h"
+#include <stdint.h>
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
---
+++ src/rsa/main.c
@@ -0,0 +1,7 @@
+#include "config.h"
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
