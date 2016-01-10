9bf1edd1e3a734143207685bf0cac82a1155943a
nitrocaster <paul-kv@yandex.ru>
2016-01-11 01:44:33 +0300

    Fix incorrect result when message>modulus.

--- src/rsa/main.c
+++ src/rsa/main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <assert.h>
 #ifndef _WIN32
 #include <unistd.h>
 #else
@@ -116,6 +117,19 @@ static int run_keygen(int argc, char *argv[])
     return 0;
 }
 
+static size_t bsize(uint32_t n)
+{
+    if (n & 0xff000000)
+        return 4;
+    if (n & 0x00ff0000)
+        return 3;
+    if (n & 0x0000ff00)
+        return 2;
+    if (n & 0x000000ff)
+        return 1;
+    return 0;
+}
+
 static int run_transform(int argc, char *argv[])
 {
     // 0    1         2   3   4
@@ -162,56 +176,44 @@ static int run_transform(int argc, char *argv[])
         puts("invalid key file.");
         return 1;
     }
-    size_t block_size = bigint_get_size(n);
-    size_t blocks_done = 0;
-    size_t plain_blocks = src_size/block_size;
-    if (mode=='d')
-        plain_blocks = plain_blocks>=1 ? plain_blocks-1 : 0;
+    size_t src_block_size, dst_block_size;
+    {
+        size_t mod_size = bigint_get_size(n);
+        size_t msg_size =
+            mod_size-(1+sizeof(uint32_t)-bsize(n->data[n->size-1]));
+        src_block_size = mode=='e' ? msg_size : mod_size;
+        dst_block_size = mode=='e' ? mod_size : msg_size;
+    }
     // decrypting: transform plain_blocks-1, apply special case for last block
     // encrypting: transform plain_blocks, apply special case for the rest
-    uint8_t *buf = malloc(block_size*64);
-    uint8_t *buf_ptr = buf;
-    size_t remain = 0;
+    size_t buf_sz = max(src_block_size, dst_block_size);
+    uint8_t *buf = malloc(buf_sz);
+    size_t zbytes = mode=='e' ? dst_block_size-src_block_size : 0;
+    assert(zbytes<=sizeof(uint32_t));
+    size_t bytes_read = 0;
     while (1)
     {
-        size_t bytes_read = fread(buf+remain, 1, block_size-remain, src);
-        if (!bytes_read) // nothing remained, proceed with the last block
-        {
-            if (buf_ptr==buf+block_size)
-                buf_ptr = buf;
+        // XXX: valid for little endian only!
+        bytes_read = fread(buf, 1, src_block_size, src);        
+        memset(buf+buf_sz-zbytes, 0, zbytes);
+        if (bytes_read!=src_block_size)
             break;
-        }
-        bytes_read += remain;
-        buf_ptr = buf;
-        size_t blocks_read = bytes_read/block_size;
-        remain = bytes_read%block_size;
-        size_t stage_block_lim = min(blocks_done+blocks_read, plain_blocks);
-        size_t blocks_staged = stage_block_lim-blocks_done;
-        for (; blocks_done<stage_block_lim; blocks_done++)
-        {
-            rsa_transform(buf_ptr, block_size, buf_ptr, exp, n);
-            buf_ptr += block_size;
-        }
-        fwrite(buf, 1, blocks_staged*block_size, dst);
-        if (remain)
-        {
-            memmove(buf, buf_ptr, remain);
-            buf_ptr = buf;
-        }
+        rsa_transform(buf, buf_sz, buf, exp, n);
+        fwrite(buf, 1, dst_block_size, dst);
     }
     // process last block
     if (mode=='d')
     {
-        rsa_transform(buf_ptr, block_size, buf_ptr, exp, n);
+        size_t src_plain_size = src_size/src_block_size*dst_block_size;
         size_t padding = 0;
-        if (dp_depad(buf_ptr, block_size, &padding)!=DP_OK ||
-            padding>src_size)
+        if (dp_depad(buf, dst_block_size, &padding)!=DP_OK ||
+            padding>src_plain_size)
         {
             puts("padding is invalid and cannot be removed.");
             return 1;
         }
         // trim to the actual plaintext size
-        if (fseek(dst, 0, SEEK_SET) || ftrim(dst, src_size-padding))
+        if (fseek(dst, 0, SEEK_SET) || ftrim(dst, src_plain_size-padding))
         {
             puts("can't set destination file size.");
             return 1;
@@ -220,20 +222,24 @@ static int run_transform(int argc, char *argv[])
     else
     {
         size_t param = 0;
-        if (dp_pad(buf_ptr, block_size, remain, &param)!=DP_MORE)
+        int pad_result = dp_pad(buf, src_block_size, bytes_read, &param);
+        if (pad_result!=DP_OK)
         {
-            puts("cannot apply padding.");
-            return 1;
+            if (pad_result!=DP_MORE)
+            {
+                puts("cannot apply padding.");
+                return 1;
+            }
+            rsa_transform(buf, buf_sz, buf, exp, n);
+            fwrite(buf, 1, buf_sz, dst);
+            if (dp_pad(buf, src_block_size, 0, &param)!=DP_OK)
+            {
+                puts("cannot apply padding.");
+                return 1;
+            }
         }
-        rsa_transform(buf_ptr, block_size, buf_ptr, exp, n);
-        fwrite(buf_ptr, 1, block_size, dst);
-        if (dp_pad(buf_ptr, block_size, 0, &param)!=DP_OK)
-        {
-            puts("cannot apply padding.");
-            return 1;
-        }
-        rsa_transform(buf_ptr, block_size, buf_ptr, exp, n);
-        fwrite(buf_ptr, 1, block_size, dst);
+        rsa_transform(buf, buf_sz, buf, exp, n);
+        fwrite(buf, 1, buf_sz, dst);
     }
     free(buf);
     bigint_free(n);
